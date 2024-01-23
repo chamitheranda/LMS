8840e8f9629dfd13584bab93b8a46ad2fc6f61c9
chamith <erandachamith322@gmail.com>
2024-01-23 18:31:48 +0530

    create spring boot application configuring swagger

---
+++ src/test/java/com/chamith/lms/springlmsapi/SpringLmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamith.lms.springlmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
