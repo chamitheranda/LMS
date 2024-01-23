6165cacc87e7098c103b8236eb67efec6787ae33
chamith <erandachamith322@gmail.com>
2024-01-23 21:15:04 +0530

    create addUser mathod and save user daya

--- src/main/java/com/chamith/lms/springlmsapi/service/UserService.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/UserService.java
@@ -1,2 +1,17 @@
-package com.chamith.lms.springlmsapi.service;public class UserService {
+package com.chamith.lms.springlmsapi.service;
+
+import com.chamith.lms.springlmsapi.dto.requestDTO.UserRequestDTO;
+import com.chamith.lms.springlmsapi.mappers.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserMapper userMapper ;
+
+    public String addUser(UserRequestDTO userReqestDTO) {
+        userMapper.insert(userReqestDTO);
+        return "User details registered";
+    }
 }
