13b29b28ec8d7410c52c5274a1bca0a19711bff0
chamith <erandachamith322@gmail.com>
2024-01-23 21:14:06 +0530

    create mapper class for user and create insert method

--- src/main/java/com/chamith/lms/springlmsapi/mappers/UserMapper.java
+++ src/main/java/com/chamith/lms/springlmsapi/mappers/UserMapper.java
@@ -1,2 +1,11 @@
-package com.chamith.lms.springlmsapi.mappers;public class UserMapper {
+package com.chamith.lms.springlmsapi.mappers;
+
+import com.chamith.lms.springlmsapi.dto.requestDTO.UserRequestDTO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into users(name, email, contact_number) values(#{name}, #{email}, #{contactNumber})")
+    void insert(UserRequestDTO userRequestDTO);
 }
