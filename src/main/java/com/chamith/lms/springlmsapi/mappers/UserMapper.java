6f32f86e5139fb1050650e8c12347ed824976c65
chamith <erandachamith322@gmail.com>
2024-01-24 10:10:05 +0530

    modify insert query

--- src/main/java/com/chamith/lms/springlmsapi/mappers/UserMapper.java
+++ src/main/java/com/chamith/lms/springlmsapi/mappers/UserMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into users(name, email, contact_number) values(#{name}, #{email}, #{contactNumber})")
+    @Insert("insert into users(name, email, contact_number , password) values(#{name}, #{email}, #{contactNumber},#{password})")
     void insert(UserRequestDTO userRequestDTO);
 }
