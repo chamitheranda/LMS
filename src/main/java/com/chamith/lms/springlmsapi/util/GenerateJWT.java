e4c03bdf3a9854f8083a14ace305f5092ca8295e
chamith <erandachamith322@gmail.com>
2024-01-24 16:13:50 +0530

    modify secret key and change signing algorithm

--- src/main/java/com/chamith/lms/springlmsapi/util/GenerateJWT.java
+++ src/main/java/com/chamith/lms/springlmsapi/util/GenerateJWT.java
@@ -8,42 +8,43 @@ import java.util.Date;
 
 @Component
 public class GenerateJWT {
-    Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
-    public String generateToken(String userEmail , String privilegeLevel){
-       long expirationTimeMillis = 86400000; // 1 day
+    private final String secretKey = "chamithEranda234chamithEranda234chamithEranda234";
 
-       String token = Jwts.builder()
-               .setSubject(userEmail)
-               .claim("privilegeLevel", privilegeLevel)
-               .setIssuedAt(new Date())
-               .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMillis))
-               .signWith(SignatureAlgorithm.HS512, secretKey)
-               .compact();
+    public String generateToken(String userEmail, String privilegeLevel) {
+        long expirationTimeMillis = 86400000; // 1 day
 
-       return token;
-   }
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        String token = Jwts.builder()
+                .setSubject(userEmail)
+                .claim("privilegeLevel", privilegeLevel)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMillis))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
+    }
 
     public AuthenticationVerification validateToken(String token) {
         try {
+            Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
             Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
+                    .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
-
             Date expiration = claims.getExpiration();
             if (expiration.before(new Date())) {
                 return new AuthenticationVerification(false);
             }
             String privilegeLevel = claims.get("privilegeLevel", String.class);
-            return new AuthenticationVerification(
-                    true ,
-                    privilegeLevel
-            ) ;
+            return new AuthenticationVerification(true, privilegeLevel);
 
         } catch (ExpiredJwtException e) {
-            return new AuthenticationVerification(false) ;
+            return new AuthenticationVerification(false);
         }
     }
 }
