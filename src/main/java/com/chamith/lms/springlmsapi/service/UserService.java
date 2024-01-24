f60d0e8d7e3b8dddd18cbc9a4c243fb1df876005
chamith <erandachamith322@gmail.com>
2024-01-24 10:11:09 +0530

    encrypt password before save into DB

--- src/main/java/com/chamith/lms/springlmsapi/service/UserService.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/UserService.java
@@ -3,15 +3,22 @@ package com.chamith.lms.springlmsapi.service;
 import com.chamith.lms.springlmsapi.dto.requestDTO.UserRequestDTO;
 import com.chamith.lms.springlmsapi.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
-    UserMapper userMapper ;
+    private UserMapper userMapper ;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder ;
 
     public String addUser(UserRequestDTO userReqestDTO) {
+        String encryptedPassword = passwordEncoder.encode(userReqestDTO.getPassword());
+        userReqestDTO.setPassword(encryptedPassword);
         userMapper.insert(userReqestDTO);
+
         return "User details registered";
     }
 }
