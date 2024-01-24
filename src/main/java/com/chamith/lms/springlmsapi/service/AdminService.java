5ba7e848e28915dbc056e6895268dc91fc5f2e78
chamith <erandachamith322@gmail.com>
2024-01-24 14:03:51 +0530

    implement updatePrivilege method logic

--- src/main/java/com/chamith/lms/springlmsapi/service/AdminService.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/AdminService.java
@@ -1,2 +1,17 @@
-package com.chamith.lms.springlmsapi.service;public class AdminService {
+package com.chamith.lms.springlmsapi.service;
+
+import com.chamith.lms.springlmsapi.mappers.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private UserMapper userMapper ;
+
+    public String updatePrivilege(String email) {
+        userMapper.updatePrivilegeLevel(email);
+        return "Successfully updated !!!!" ;
+    }
 }
