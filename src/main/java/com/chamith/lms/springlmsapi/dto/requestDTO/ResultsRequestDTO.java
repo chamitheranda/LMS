b1482d4e01490542685b4f2d120e5e01f46dd800
chamith <erandachamith322@gmail.com>
2024-01-24 19:05:20 +0530

    create ResultRequestDTO for catch data comes from client

--- src/main/java/com/chamith/lms/springlmsapi/dto/requestDTO/ResultsRequestDTO.java
+++ src/main/java/com/chamith/lms/springlmsapi/dto/requestDTO/ResultsRequestDTO.java
@@ -1,2 +1,15 @@
-package com.chamith.lms.springlmsapi.dto.requestDTO;public class ResultsRequestDTO {
+package com.chamith.lms.springlmsapi.dto.requestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResultsRequestDTO {
+    private String student_name ;
+    private String subject;
+    private String results;
+    private String email;
 }
