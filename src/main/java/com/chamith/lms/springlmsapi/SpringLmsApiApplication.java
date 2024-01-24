e82d57882a1a2bf96c8a18b093b656429c886cd7
chamith <erandachamith322@gmail.com>
2024-01-24 10:06:48 +0530

    enable web security using anotation

--- src/main/java/com/chamith/lms/springlmsapi/SpringLmsApiApplication.java
+++ src/main/java/com/chamith/lms/springlmsapi/SpringLmsApiApplication.java
@@ -2,9 +2,11 @@ package com.chamith.lms.springlmsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableWebSecurity
 @SpringBootApplication
 public class SpringLmsApiApplication {
 
