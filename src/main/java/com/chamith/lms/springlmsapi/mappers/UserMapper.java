5314bb39bd2f44cbe313baa75e6be7fb9862fb59
chamith <erandachamith322@gmail.com>
2024-01-24 11:35:53 +0530

    create selectPasswordByEmail method

--- src/main/java/com/chamith/lms/springlmsapi/mappers/UserMapper.java
+++ src/main/java/com/chamith/lms/springlmsapi/mappers/UserMapper.java
@@ -3,9 +3,14 @@ package com.chamith.lms.springlmsapi.mappers;
 import com.chamith.lms.springlmsapi.dto.requestDTO.UserRequestDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
     @Insert("insert into users(name, email, contact_number , password) values(#{name}, #{email}, #{contactNumber},#{password})")
     void insert(UserRequestDTO userRequestDTO);
+
+    @Select("SELECT password FROM users WHERE email = #{email}")
+    String selectPasswordByEmail(String email);
 }
