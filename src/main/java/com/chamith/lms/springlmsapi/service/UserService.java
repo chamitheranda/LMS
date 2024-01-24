13e0c42aceabc216495f2f6173fcc059291effa4
chamith <erandachamith322@gmail.com>
2024-01-24 11:37:23 +0530

    implement logic for signin process

--- src/main/java/com/chamith/lms/springlmsapi/service/UserService.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/UserService.java
@@ -1,8 +1,12 @@
 package com.chamith.lms.springlmsapi.service;
 
+import com.chamith.lms.springlmsapi.dto.requestDTO.SignInRequestDTO;
 import com.chamith.lms.springlmsapi.dto.requestDTO.UserRequestDTO;
 import com.chamith.lms.springlmsapi.mappers.UserMapper;
+import com.chamith.lms.springlmsapi.util.GenerateJWT;
+import com.chamith.lms.springlmsapi.util.SingInCredientials;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +18,27 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder ;
 
+    @Autowired
+    private GenerateJWT generateJWT  ;
+
     public String addUser(UserRequestDTO userReqestDTO) {
         String encryptedPassword = passwordEncoder.encode(userReqestDTO.getPassword());
         userReqestDTO.setPassword(encryptedPassword);
         userMapper.insert(userReqestDTO);
 
         return "User details registered";
+    }
+
+    public SingInCredientials signIn(SignInRequestDTO signInRequestDTO) {
+        String storedPassword = userMapper.selectPasswordByEmail(signInRequestDTO.getEmail());
+        if(passwordEncoder.matches(signInRequestDTO.getPassword() , storedPassword)){
+            String jwtToken = generateJWT.generateToken(signInRequestDTO.getEmail());
+            return new SingInCredientials(
+                    jwtToken,
+                    HttpStatus.OK
+            ) ;
+        }else{
+            return new SingInCredientials(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
