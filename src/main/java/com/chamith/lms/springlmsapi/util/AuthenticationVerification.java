115ea1d88119f9b829e3c47e56660f58f6189781
chamith <erandachamith322@gmail.com>
2024-01-24 14:07:55 +0530

    create hold auhthentication status and privilegeLevel

--- src/main/java/com/chamith/lms/springlmsapi/util/AuthenticationVerification.java
+++ src/main/java/com/chamith/lms/springlmsapi/util/AuthenticationVerification.java
@@ -1,2 +1,16 @@
-package com.chamith.lms.springlmsapi.util;public class AuthenticationVerification {
+package com.chamith.lms.springlmsapi.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class AuthenticationVerification {
+    private boolean authenticationStatus ;
+    private String privilegeLevel ;
+
+    public AuthenticationVerification(boolean authenticationStatus) {
+        this.authenticationStatus = authenticationStatus;
+    }
 }
