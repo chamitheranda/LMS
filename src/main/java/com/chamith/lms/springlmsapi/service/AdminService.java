9a0430bbd2ff9a8198a2e2b1c309c680fda9b5ff
chamith <erandachamith322@gmail.com>
2024-01-24 16:12:34 +0530

    extend logic for email doesnt exist scenario

--- src/main/java/com/chamith/lms/springlmsapi/service/AdminService.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/AdminService.java
@@ -1,7 +1,10 @@
 package com.chamith.lms.springlmsapi.service;
 
 import com.chamith.lms.springlmsapi.mappers.UserMapper;
+import com.chamith.lms.springlmsapi.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +13,24 @@ public class AdminService {
     @Autowired
     private UserMapper userMapper ;
 
-    public String updatePrivilege(String email) {
-        userMapper.updatePrivilegeLevel(email);
-        return "Successfully updated !!!!" ;
+    public ResponseEntity<StandardResponse> updatePrivilege(String email) {
+        if(userMapper.doesEmailExist(email)){
+            userMapper.updatePrivilegeLevel(email);
+            return new ResponseEntity<>(
+                        new StandardResponse(
+                                200,
+                                "Update Privilege",
+                                "Update successfully !!!!"
+                        ), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(
+                        new StandardResponse(
+                                204,
+                                "email not found",
+                                "update failed !!!!"
+                        ), HttpStatus.NOT_FOUND);
+            }
+        }
+
     }
-}
+
