f36dbad22c6e0e83c9c68239d48752f958a246ad
chamith <erandachamith322@gmail.com>
2024-01-24 19:06:22 +0530

    create addResults method

--- src/main/java/com/chamith/lms/springlmsapi/mappers/ResultMapper.java
+++ src/main/java/com/chamith/lms/springlmsapi/mappers/ResultMapper.java
@@ -1,2 +1,11 @@
-package com.chamith.lms.springlmsapi.mappers;public class ResultMapper {
+package com.chamith.lms.springlmsapi.mappers;
+
+import com.chamith.lms.springlmsapi.dto.requestDTO.ResultsRequestDTO;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface ResultMapper {
+
+    @Insert("insert into results(student_name, email, subject , results) values(#{student_name}, #{email}, #{subject},#{results})")
+    void addResults(ResultsRequestDTO resultsRequestDTO);
 }
