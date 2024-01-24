542f623fedc5362e41d8d491373daaa8e8be20f4
chamith <erandachamith322@gmail.com>
2024-01-24 11:35:08 +0530

    create signInRequest DTO

--- src/main/java/com/chamith/lms/springlmsapi/dto/requestDTO/SignInRequestDTO.java
+++ src/main/java/com/chamith/lms/springlmsapi/dto/requestDTO/SignInRequestDTO.java
@@ -1,2 +1,13 @@
-package com.chamith.lms.springlmsapi.dto.requestDTO;public class SignInRequestDTO {
+package com.chamith.lms.springlmsapi.dto.requestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SignInRequestDTO {
+    private String email ;
+    private String password ;
 }
