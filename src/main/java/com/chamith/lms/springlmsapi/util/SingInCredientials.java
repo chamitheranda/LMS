020254bd6b182b20e291b62b2ec1c596ed2d8f3d
chamith <erandachamith322@gmail.com>
2024-01-24 11:38:54 +0530

    create class for store jwt and httpstatus

--- src/main/java/com/chamith/lms/springlmsapi/util/SingInCredientials.java
+++ src/main/java/com/chamith/lms/springlmsapi/util/SingInCredientials.java
@@ -1,2 +1,18 @@
-package com.chamith.lms.springlmsapi.util;public class SingInCredientials {
+package com.chamith.lms.springlmsapi.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SingInCredientials {
+    private String token ;
+    private HttpStatus httpStatus ;
+
+    public SingInCredientials(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
 }
