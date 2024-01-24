8fc0a99f5fc82d5d9b121e718620d80a1961222c
chamith <erandachamith322@gmail.com>
2024-01-24 10:07:49 +0530

    override configure method

--- src/main/java/com/chamith/lms/springlmsapi/config/SecurityConfig.java
+++ src/main/java/com/chamith/lms/springlmsapi/config/SecurityConfig.java
@@ -1,2 +1,26 @@
-package com.chamith.lms.springlmsapi.config;public class SecurityConfig {
+package com.chamith.lms.springlmsapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
+                .permitAll();
+        http.csrf().disable();
+    }
 }
