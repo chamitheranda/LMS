8a1c0b5de31679bbf92d251c25d7d4af80b57701
chamith <erandachamith322@gmail.com>
2024-01-26 18:49:41 +0530

    create unit test cases for remove user endpoint

--- src/test/java/com/chamith/lms/springlmsapi/testControllers/adminControllerTest/TestDeleteUser.java
+++ src/test/java/com/chamith/lms/springlmsapi/testControllers/adminControllerTest/TestDeleteUser.java
@@ -1,2 +1,88 @@
-package com.chamith.lms.springlmsapi.testControllers.adminControllerTest;public class TestDeleteUser {
+package com.chamith.lms.springlmsapi.testControllers.adminControllerTest;
+
+import com.chamith.lms.springlmsapi.controller.AdminController;
+import com.chamith.lms.springlmsapi.service.AdminService;
+import com.chamith.lms.springlmsapi.util.AuthenticationVerification;
+import com.chamith.lms.springlmsapi.util.GenerateJWT;
+import com.chamith.lms.springlmsapi.util.StandardResponse;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestDeleteUser {
+
+    @Mock
+    private GenerateJWT generateJWT;
+
+    @Mock
+    private AdminService adminService;
+
+    @InjectMocks
+    private AdminController adminController;
+
+    private String validAdminToken;
+    private String invalidToken;
+    private String validNonAdminToken;
+
+    @Before
+    public void setup() {
+        validAdminToken = "valid_admin_token";
+        invalidToken = "invalid_token";
+        validNonAdminToken = "valid_non_admin_token";
+    }
+
+
+    @Test
+    public void testRemoveUserSuccess() {
+        when(generateJWT.validateToken(validAdminToken)).
+                thenReturn(new AuthenticationVerification(true , "admin"));
+
+        when(generateJWT.extractSubject(validAdminToken)).thenReturn("admin@example.com");
+
+        when(adminService.deleteUser("admin@example.com")).thenReturn(
+                new ResponseEntity<>(
+                new StandardResponse(
+                        200,
+                        "User is a admin",
+                        "User can't delete !!!!"
+                ), HttpStatus.OK));
+
+        ResponseEntity<StandardResponse> response = adminController.removeUser(validAdminToken);
+
+        verify(generateJWT, times(1)).validateToken(validAdminToken);
+        verify(generateJWT).extractSubject(validAdminToken);
+        verify(adminService).deleteUser("admin@example.com");
+        assertResponseStatus(response, HttpStatus.OK);
+    }
+
+    @Test
+    public void testRemoveUserAccessDenied() {
+        when(generateJWT.validateToken(invalidToken)).thenReturn(new AuthenticationVerification(false));
+
+        ResponseEntity<StandardResponse> response = adminController.removeUser(invalidToken);
+
+        verify(generateJWT).validateToken(invalidToken);
+        assertResponseStatus(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    public void testRemoveUserUnauthorized() {
+        when(generateJWT.validateToken(validNonAdminToken)).
+                thenReturn(new AuthenticationVerification(true , "none"));
+
+        ResponseEntity<StandardResponse> response = adminController.removeUser(validNonAdminToken);
+
+        verify(generateJWT).validateToken(validNonAdminToken);
+        assertResponseStatus(response, HttpStatus.FORBIDDEN);
+    }
+    private void assertResponseStatus(ResponseEntity<StandardResponse> response, HttpStatus expectedStatus) {
+        assert response.getStatusCode().equals(expectedStatus);
+    }
 }
