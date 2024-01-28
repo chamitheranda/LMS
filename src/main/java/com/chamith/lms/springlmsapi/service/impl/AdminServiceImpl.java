a2760c109b6e1eb31741d66381d3b5734bad074c
chamith <erandachamith322@gmail.com>
2024-01-28 13:25:36 +0530

    add log for AdminServiceImpl

--- src/main/java/com/chamith/lms/springlmsapi/service/impl/AdminServiceImpl.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/impl/AdminServiceImpl.java
@@ -5,6 +5,8 @@ import com.chamith.lms.springlmsapi.mappers.ResultMapper;
 import com.chamith.lms.springlmsapi.mappers.UserMapper;
 import com.chamith.lms.springlmsapi.service.AdminService;
 import com.chamith.lms.springlmsapi.util.StandardResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +24,13 @@ public class AdminServiceImpl implements AdminService {
     @Autowired
     private EnrolledCourseMapper enrolledCourseMapper ;
 
+    private static final Logger logger = LoggerFactory.getLogger(AdminServiceImpl.class);
+
     @Override
     public ResponseEntity<StandardResponse> updatePrivilege(String email) {
         if(userMapper.doesEmailExist(email) ){
                 userMapper.updatePrivilegeLevel(email);
+                logger.info("Update user privilege : "+email );
                 return new ResponseEntity<>(
                         new StandardResponse(
                                 200,
@@ -33,6 +38,7 @@ public class AdminServiceImpl implements AdminService {
                                 "Update successfully !!!!"
                         ), HttpStatus.OK);
         }else {
+                logger.warn("Email = "+email+"NotFound");
                 return new ResponseEntity<>(
                         new StandardResponse(
                                 204,
@@ -57,7 +63,7 @@ public class AdminServiceImpl implements AdminService {
                     }else {
                         userMapper.deleteUserByEmailFromUsers(email);
                     }
-
+                    logger.info("Delete User from Results by email : "+email);
                     return new ResponseEntity<>(
                             new StandardResponse(
                                     200,
@@ -66,6 +72,7 @@ public class AdminServiceImpl implements AdminService {
                             ), HttpStatus.OK);
                 }catch (Exception e) {
                     e.printStackTrace();
+                    logger.error("Internal Server Error");
                     return new ResponseEntity<>(
                             new StandardResponse(
                                     500,
@@ -75,6 +82,7 @@ public class AdminServiceImpl implements AdminService {
                 }
 
         }else {
+            logger.info("User Not found email = "+email);
             return new ResponseEntity<>(
                     new StandardResponse(
                             204,
@@ -88,6 +96,7 @@ public class AdminServiceImpl implements AdminService {
     public ResponseEntity<StandardResponse> updateResults(String email , String result) {
         if(userMapper.doesEmailExist(email) ){
                 userMapper.updateResult(result);
+                logger.info("Result Update Successfully email : "+email);
                 return new ResponseEntity<>(
                         new StandardResponse(
                                 200,
@@ -95,6 +104,7 @@ public class AdminServiceImpl implements AdminService {
                                 "Result Update successfully !!!!"
                         ), HttpStatus.OK);
         }else {
+               logger.warn("Email = "+email+"NotFound");
                return new ResponseEntity<>(
                         new StandardResponse(
                                 204,
