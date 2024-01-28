dce9728f17fb0021b7f3a4b4b75e794a351cf164
chamith <erandachamith322@gmail.com>
2024-01-28 13:36:34 +0530

    add log for UserServiceImpl

--- src/main/java/com/chamith/lms/springlmsapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.chamith.lms.springlmsapi.dto.requestDTO.EnrollRequestDTO;
 import com.chamith.lms.springlmsapi.mappers.UserMapper;
 import com.chamith.lms.springlmsapi.service.UserService;
 import com.chamith.lms.springlmsapi.util.StandardResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +16,13 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper ;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Override
     public ResponseEntity<StandardResponse> enroll(EnrollRequestDTO enrollRequestDTO, String email) {
         if(userMapper.doesSubjectExist(email , enrollRequestDTO.getSubject())
                 && userMapper.doesEmailExist(email)){
+            logger.warn("Already enrolled for subject "+enrollRequestDTO.getSubject());
             return new ResponseEntity<>(
                     new StandardResponse(
                             417,
@@ -26,6 +31,7 @@ public class UserServiceImpl implements UserService {
                     ), HttpStatus.EXPECTATION_FAILED);
         }else {
             userMapper.addCourse(enrollRequestDTO.getSubject() , email);
+            logger.info("Enrolled course = "+enrollRequestDTO.getSubject()+" user = "+email);
             return new ResponseEntity<>(
                     new StandardResponse(
                             200,
