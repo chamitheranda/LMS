eb2cb80f053b5dd55ebb8207bf978169c226d62f
chamith <erandachamith322@gmail.com>
2024-01-28 13:14:31 +0530

    add dependency for stablish logging mechanism

--- src/main/java/com/chamith/lms/springlmsapi/service/impl/AuthServiceImpl.java
+++ src/main/java/com/chamith/lms/springlmsapi/service/impl/AuthServiceImpl.java
@@ -6,6 +6,9 @@ import com.chamith.lms.springlmsapi.mappers.UserMapper;
 import com.chamith.lms.springlmsapi.service.AuthService;
 import com.chamith.lms.springlmsapi.util.GenerateJWT;
 import com.chamith.lms.springlmsapi.util.SingInCredientials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,13 +30,19 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     private UserMapper userMapper ;
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthServiceImpl.class);
 
     @Override
     public String addUser(UserRequestDTO userReqestDTO) {
         String encryptedPassword = passwordEncoder.encode(userReqestDTO.getPassword());
         userReqestDTO.setPassword(encryptedPassword);
         userMapper.insert(userReqestDTO);
-
+        MDC.put("user.email", userReqestDTO.getEmail());
+        try {
+            logger.info("User successfully SignUp "+userReqestDTO.getEmail());
+        }finally {
+            MDC.clear();
+        }
         return "User details registered";
     }
 
@@ -43,6 +52,12 @@ public class AuthServiceImpl implements AuthService {
         if(passwordEncoder.matches(signInRequestDTO.getPassword() , storedPassword)){
             String jwtToken = generateJWT.generateToken(signInRequestDTO.getEmail() ,
                     userMapper.selectPrivilegeLevelByEmail(signInRequestDTO.getEmail()));
+                    MDC.put("email", signInRequestDTO.getEmail());
+                    try {
+                        logger.info("User successfully Sign IN " + signInRequestDTO.getEmail());
+                    }finally {
+                        MDC.clear();
+                    }
             return new SingInCredientials(
                     jwtToken,
                     HttpStatus.OK
